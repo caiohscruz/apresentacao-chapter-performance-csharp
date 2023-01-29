6edc0e1c1e085db77181779ca415c8c4cec733cb
CaioHSCruz <caiohscruz@hotmail.com>
2023-01-29 20:14:33 -0300

    First commit

---
+++ CSharpPerfomance/Avoid_Boxing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Avoid_Boxing
+{
+    class Program
+    {
+        public const int ITERATIONS = 1000000;
+
+        public static void WithoutBoxingTestMethod(int value)
+        {
+            // do something with the value
+        } 
+        
+        public static void WithBoxingTestMethod(object value)
+        {
+            // do something with the value
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < ITERATIONS; i++)
+            {
+                //WithoutBoxingTestMethod(i);
+                WithBoxingTestMethod(i);
+            }
+        }
+    }
+}
